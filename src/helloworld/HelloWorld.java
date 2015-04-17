f9124fd4125154dc0c346cc8925cd6068de0e1eb
Jason <Jason@v1021-wn-189-83.campus-dynamic.uwaterloo.ca>
2015-04-17 15:40:32 -0400

    This is the version of the code I want to share.

---
+++ src/helloworld/HelloWorld.java
@@ -0,0 +1,14 @@
+
+package helloworld;
+
+
+public class HelloWorld {
+
+    
+    public static void main(String[] args) {
+        System.out.println("Hey, this is Mr. Schattman here");
+        
+        //Isn't it cool how you can upload code to GitHub directly from NetBeans?
+    }
+    
+}
